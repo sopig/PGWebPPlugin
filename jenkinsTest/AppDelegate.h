c9625435130fb1df439e98adbed631fa1c8ba4e3
张正超 <once@tctios.local>
2015-12-20 10:20:22 +0800

    Initial Commit

---
+++ jenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jenkinsTest
+//
+//  Created by 张正超 on 15/12/20.
+//  Copyright © 2015年 张正超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jenkinsTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jenkinsTest
+//
+//  Created by 张正超 on 15/12/20.
+//  Copyright © 2015年 张正超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
